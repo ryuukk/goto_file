b882b6ff25f18528f0caa1000a61e594f04aa4d8
ryuukk <44361234+ryuukk@users.noreply.github.com>
2023-10-02 06:06:53 +0200

    Screw caching

--- goto.py
+++ goto.py
@@ -2,37 +2,48 @@ import sublime
 import sublime_plugin
 import os
 import fnmatch
+import re
 
 # command: goto_file
 class GotoFileCommand(sublime_plugin.WindowCommand):
     panelFlags = sublime.MONOSPACE_FONT | sublime.KEEP_OPEN_ON_FOCUS_LOST
     filefilter = "*" # TODO: make it configurable
     ignoreFolders = [ ".git", ".svn", ".build", ".cache" ]
+    ignoreExts = [ ".pdb", ".obj", ".lib", ".exp", ".exe", ".wasm", ".rdbg" ]
     items = []
     paths = []
-    times = {}
-    cachedItems = {}
-    cachedPaths = {}
-    usePrefix = False
 
-    def cache_folder(self, folder, t):
-        self.times[folder] = t
+    def cache_folder(self, folder):
         baseFolder = os.path.basename(folder)
-        if baseFolder in self.ignoreFolders:
-            return
-        for dirpath, dirnames, filenames in os.walk(folder):
+        print("cache: ", folder, "->", baseFolder)
+        for it in self.ignoreFolders:
+            if baseFolder in it:
+                return
+        for dirpath, dirnames, filenames in os.walk(folder, topdown=True):
             if not filenames:
                 continue
+            ignore = False
+            for it in self.ignoreFolders:
+                if it in dirpath:
+                    ignore = True
+                    break
+            if ignore:
+                continue
+
             filtered = fnmatch.filter(filenames, self.filefilter)
             if filtered:
                 for file in filtered:
-                    path = "{}/{}".format(dirpath, file)
-                    if self.usePrefix:
-                        item = sublime.QuickPanelItem(baseFolder + "> " + file, path)
-                    else:
-                        item = sublime.QuickPanelItem(file, path)
-                    self.cachedItems[folder].insert(0, item)
-                    self.cachedPaths[folder].insert(0, path)
+                    ignore = False
+                    for it in self.ignoreExts:
+                        if file.endswith(it):
+                            ignore = True
+                    if ignore:
+                        continue
+                    path = "{}{}{}".format(dirpath, os.path.sep, file)
+                    localPath = path.replace(folder + os.path.sep, '')
+                    item = sublime.QuickPanelItem(trigger=localPath, details=[file, path], annotation=baseFolder)
+                    self.items.insert(0, item)
+                    self.paths.insert(0, path)
 
     def run(self):
         print("goto_file: running the command")
@@ -44,39 +55,8 @@ class GotoFileCommand(sublime_plugin.WindowCommand):
             self.usePrefix = True
 
         for folder in folders:
-            t = os.stat(folder).st_mtime
+            self.cache_folder(folder)
 
-            if folder in self.times:
-                cachedTime = self.times[folder]
-                if t != cachedTime:
-                    print("goto_file: cache outdated {}:{}".format(cachedTime, t))
-                    self.cachedItems[folder] = []
-                    self.cachedPaths[folder] = []
-                    self.cache_folder(folder, t)
-                    for c in self.cachedItems[folder]:
-                        self.items.insert(0, c)
-                    for c in self.cachedPaths[folder]:
-                        self.paths.insert(0, c)
-                else:
-                    print("goto_file: cache up to date for: ", folder)
-                    for c in self.cachedItems[folder]:
-                        self.items.insert(0, c)
-                    for c in self.cachedPaths[folder]:
-                        self.paths.insert(0, c)
-                    continue
-
-            else:
-                print("goto_file: folder: ", folder," not cached")
-                self.times[folder] = t
-                self.cachedItems[folder] = []
-                self.cachedPaths[folder] = []
-                self.cache_folder(folder, t)
-                for c in self.cachedItems[folder]:
-                    self.items.insert(0, c)
-                for c in self.cachedPaths[folder]:
-                    self.paths.insert(0, c)
-
-        
         self.window.show_quick_panel(self.items, self.check_selection, self.panelFlags, placeholder= "Enter a Path name")
         print("goto_file: items len: {}".format(len(self.items)))
 
